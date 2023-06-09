8c0b7dfb28253902d1aea608a6d3d4e5bc3e48e5
Ryan Hasler <rhasler004@gmail.com>
2023-06-09 15:05:20 -0400

    events listened to

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 // Menu data structure
 var menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
-  ];
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
 
 const mainE1 = document.querySelector('main');
 const root = document.querySelector(':root');
 mainE1.style.backgroundColor = getComputedStyle(document.querySelector(':root')).getPropertyValue('--main-bg');
 
-//solution without innerHTML
-mainE1.append(document.createElement('h1'));
-mainE1.removeChild(mainE1.firstChild);
-mainE1.lastChild.textContent = "DOM Manipulation";
+
+changeHeader("DOM Manipulation");
 
 mainE1.className = "flex-ctr";
 
@@ -27,4 +36,93 @@ for (link of menuLinks) {
     newLink.setAttribute('href', link.href);
     newLink.textContent = link.text;
     topMenuE1.append(newLink);
+}
+
+subMenuE1 = document.querySelector('#sub-menu');
+subMenuE1.style.height = '100%';
+subMenuE1.style.backgroundColor = getComputedStyle(document.querySelector(':root')).getPropertyValue('--sub-menu-bg');
+subMenuE1.className = 'flex-around';
+
+subMenuE1.style.position = 'absolute';
+subMenuE1.style.top = '0';
+
+topMenuLinks = document.querySelectorAll('a');
+let showingSubMenu = false;
+
+topMenuE1.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if(evt.target.tagName != 'A') {
+    return;
+  }
+  
+  //if submenu is active, toggle it off
+  if(evt.target.classList.contains('active')) {
+    evt.target.classList.remove('active');
+    showingSubMenu = false;
+    subMenuE1.style.top = '0';
+    return;
+  }
+
+  //otherwise show the submenu if there is one
+  for(link of topMenuLinks) {
+    link.classList.remove('active');
+  }
+  evt.target.classList.add('active');
+
+  let index = getIndex(evt);
+  if(menuLinks[index].subLinks) {
+    showingSubMenu = true;
+  }
+  else {
+    showingSubMenu = false;
+    changeHeader("About");
+  }
+
+  if(showingSubMenu == true) {
+    let index = getIndex(evt);
+    let subLinks = menuLinks[index].subLinks;
+    buildSubMenu(subLinks);
+    subMenuE1.style.top = '100%';
+  }
+  else {
+    subMenuE1.style.top = '0';
+  }
+  
+
+});
+
+subMenuE1.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (evt.target.tagName != 'A') {
+    return;
+  }
+  console.log(evt.target);
+
+  showingSubMenu = false;
+  subMenuE1.style.top = '0';
+  changeHeader(evt.target.textContent);
+});
+
+
+function buildSubMenu(subLinks) {
+  while(subMenuE1.firstChild) {
+    subMenuE1.removeChild(subMenuE1.lastChild);
+  }
+  for(link of subLinks) {
+    let newLink = document.createElement('a');
+    newLink.setAttribute('href', link.href);
+    newLink.textContent = link.text;
+    subMenuE1.append(newLink);
+  }
+}
+
+function getIndex(evt) {
+  return menuLinks.findIndex(obj => obj.text == evt.target.textContent);
+}
+
+function changeHeader(text) {
+  //solution without innerHTML
+  mainE1.append(document.createElement('h1'));
+  mainE1.removeChild(mainE1.firstChild);
+  mainE1.lastChild.textContent = text;
 }
